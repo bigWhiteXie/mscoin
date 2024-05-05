23d1c5a6f5901324f14f5d991c92a7dc1fcad0d4
codeXie <codeXie@163.com>
2024-05-05 21:16:21 +0800

    "feat:代码完善"

---
+++ ucenter-api/internal/config/config.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"github.com/zeromicro/go-zero/core/stores/redis"
+	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/zrpc"
+)
+
+type Config struct {
+	rest.RestConf
+	Rpcclient zrpc.RpcClientConf
+	Mysql     Mysql
+	Redis     redis.RedisConf
+}
+
+type Mysql struct {
+	DataSource string
+}
---
+++ ucenter-api/internal/logic/loginc_logic.go
@@ -0,0 +1,44 @@
+package logic
+
+import (
+	"context"
+	"github.com/jinzhu/copier"
+	"grpc-common/ucenter/login"
+
+	"ucenter-api/internal/svc"
+	"ucenter-api/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type LoginLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic {
+	return &LoginLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *LoginLogic) Login(req *types.LoginReq) (resp *types.LoginRes, err error) {
+	loginRpc := l.svcCtx.LoginRpc
+	rpcReq := &login.LoginReq{}
+	if err = copier.Copy(rpcReq, req); err != nil {
+		return nil, err
+	}
+
+	res, err := loginRpc.Login(l.ctx, rpcReq)
+	if err != nil {
+		return nil, err
+	}
+	resp = &types.LoginRes{}
+	if err = copier.Copy(resp, res); err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
